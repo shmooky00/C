120f5c12457fcbba64260a3e7f02114c655dbc26
Anthony Cassero <casseroant@outlook.com>
2025-02-03 22:51:46 -0500

    Add files via upload

---
+++ russiandoll type thing/russiandoll type thing/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace russiandoll_type_thing
+{
+    public class Doll
+    {
+        public string size { get; set; }
+        public Doll child { get; set; } //type prop, then tab
+
+        public Doll(string size, Doll child) //properties are lowercase, can also use underscore
+        {
+            this.size = size;
+            this.child = child;
+        }
+
+        //since this is an object, doll, make it able to do a verb
+        //such as open
+
+        public Doll Open() //verb on a noun, doesnt take an input 
+        {
+            if (this.child == null) //if at the smallest
+            {
+                Console.WriteLine($"Cannot open {this.size}");
+                
+                return null;
+            }
+            else
+            {
+                Console.WriteLine($"Opened {size}");
+
+                return this.child;
+            }
+
+            
+        }
+    }
+}
---
+++ russiandoll type thing/russiandoll type thing/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace russiandoll_type_thing
+{
+
+    //need doll (class)
+    //need to be able to do verbs 
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var smallest = new Doll("smallest", null); //choose smallest doll first
+
+            var medium = new Doll("medium", smallest); //fit the small into medium
+
+            var largest = new Doll("largest", medium); //fit all inside of the largest
+
+            //now make program stop after opening all of them
+
+            OpenAllDolls(largest);
+
+        }
+
+
+        //time complexity: O(N)
+        static void OpenAllDolls(Doll doll)
+        {
+            //base case 
+            if (doll.child == null)
+            {
+                //done
+                doll.Open(); //since wouldnt open smallest orignally.
+                             //now will display that smallest cannot be opened
+
+            }
+            else
+            {
+                //recursive case. also returns a value
+                var dollThatCameOutOfOpenedDoll = doll.Open(); //teach doll how to open 
+                                                               //smallest doll that comes out
+                OpenAllDolls(dollThatCameOutOfOpenedDoll);
+
+            }
+                                               
+        }
+    }
+
+   
+    
+}
+
+
