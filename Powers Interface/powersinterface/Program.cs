92939d7027812bff7fe4c967f0abbb359415169a
Anthony Cassero <casseroant@outlook.com>
2025-02-03 23:42:24 -0500

    Add files via upload

---
+++ Powers Interface/powersinterface/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powersinterface
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            FancyMath fancyMath = new FancyMath();
+            int intValue = 5;
+            Console.WriteLine($"Int Value: {intValue}, Square: {fancyMath.square(intValue)}, Cube: {fancyMath.cube(intValue)}" );
+
+            fancyMath = new FancyMath();
+            double dV = 4.5;
+            Console.WriteLine($"Double Value: {dV}, Square: {fancyMath.square(dV)}, Cube: {fancyMath.cube(dV)}");
+
+            fancyMath = new FancyMath();
+            double sqrtV = 16.0;
+            Console.WriteLine($"Int Value: {sqrtV}, Sqrt Value: {fancyMath.sqrt(sqrtV)}");
+
+            Console.ReadKey();
+        }
+
+        public interface IPowers
+        {
+            int square(int x);
+            double square(double x);
+
+            int cube(int x);
+            double cube(double x);
+
+            double sqrt(double x);
+
+        } 
+
+        public class FancyMath : IPowers
+        {
+            public int square(int x)
+            {
+                return x*x ;
+
+            }
+
+            public double square(double x)
+            {
+                return x*x ;
+            }
+
+            public int cube(int x)
+            {
+                return x*x ;
+            }
+
+            public double cube(double x)
+            {
+                return x*x ;
+            }
+            public double sqrt(double x)
+            {
+                return Math.Sqrt(x) ;
+            }
+
+            
+
+        }
+        
+
+        
+    }
+}
---
+++ Prime Number Finder/prime number/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prime_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number;
+
+            if (int.TryParse(textBox1.Text, out number))
+            {
+                if (IsPrime(number))
+                {
+                    primeLabel.Text = $"{number} is a prime number.";
+                }
+                else
+                {
+                    primeLabel.Text = $"{number} is not a prime number.";
+                }
+            }
+            else
+            {
+                primeLabel.Text = "Please enter a valid integer.";
+            }
+
+        }
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+           
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+
+        }
+
+    }
+}
