f32ce7c66e381f6b4d5ff7da1aa12dc430ce7f77
Anthony Cassero <casseroant@outlook.com>
2025-02-03 23:49:16 -0500

    Add files via upload

---
+++ Pop/snap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1); //sincce stack is made as an iint, can only push int 
+            stack.Push(30); 
+
+            var result = stack.Pop(); //returns the item that pops, last thing in the push
+
+            Console.WriteLine(result);
+
+            var myList = new List<int>();
+
+            var queue = new Queue<int>();
+        }
+    }
+}
